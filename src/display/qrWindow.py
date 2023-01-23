ddc10a88fad87ab19d450571bb6a5fcdb63fb87e
wesley <wesley.mprog@gmail.com>
2023-01-23 17:54:19 -0300

    Create window with wxpython

--- src/display/qrWindow.py
+++ src/display/qrWindow.py
@@ -1,26 +1,11 @@
-import os
-os.environ['KIVY_NO_ARGS'] = '1'
-from kivymd.app import MDApp
-from kivy.lang import Builder
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
-from kivy.core.image import Image as CoreImage
-from kivymd.uix.menu import MDDropdownMenu
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.properties import BooleanProperty, StringProperty
-from kivymd.uix.list import IRightBodyTouch, OneLineAvatarIconListItem
+import wx
 from io import BytesIO
-from utils import ROOT_DIR, config
+# from utils import ROOT_DIR, config
 from PIL import Image
 
-from .menu_items import settings_items
 
+# DISPLAY_DIR = ROOT_DIR.joinpath('display')
 
-Window.size = (400, 500)
-DISPLAY_DIR = ROOT_DIR.joinpath('display')
-kv_file = DISPLAY_DIR.joinpath('kvFiles', 'qrWindow.kv')
-
-Builder.load_file(str(kv_file))
 
 def pillImg_to_texture(img):
     """Converts a PIL.Image into a kivy texture
@@ -32,43 +17,40 @@ def pillImg_to_texture(img):
     kimg_data = BytesIO(data.read())
     return CoreImage(kimg_data, ext='png').texture
 
+ 
 
-class QrFrame(Widget):
-    """Container for the qrCode image
-    """
-    def __init__(self, code, **kwargs):
-        super(QrFrame, self).__init__(**kwargs)
-        self.ids.txt.text = "DON'T close this window\n"+ \
-                            "until download complete"
-        self.ids.img.texture = pillImg_to_texture(code)
-
-
-class RightContentCls(IRightBodyTouch, MDBoxLayout):
-    pass
-
-
-class ListCheckBoxItem(OneLineAvatarIconListItem):
-    left_icon = StringProperty()
-    active = BooleanProperty(config.getboolean('saving', 'ZIP_FILES'))
-        
-    def on_toggle(self, instance, value):
-        self.active = not self.active
-        config.set('saving', 'ZIP_FILES', str(self.active))
-
-
-class QrApp(MDApp):
-    def __init__(self, code, **kwargs):
-        super(QrApp, self).__init__(**kwargs)
+class QrApp(wx.Frame):
+    def __init__(self, code: Image, *args, **kwargs):
+        super(QrApp, self).__init__(*args, **kwargs)
         self._code = code
-        self.qrFrame = QrFrame(self._code)
-        self.menu = MDDropdownMenu(caller=self.qrFrame.ids.config_btn, items=settings_items, width_mult=4,)
+        self._panel = wx.Panel(self)
+        self._text_warning = self._make_text_warning()
+        self._sizer = self._create_sizer()
+        self._sizer.Add(self._text_warning, wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25))
+        self._bitmap = self.static_bitmap_from_pil_image(code)
+        self._sizer.Add(self._bitmap)
 
+    def _make_text_warning(self):
+        text = wx.StaticText(self._panel, label="Don't Close this window until download complete.")
+        font = text.GetFont()
+        font.PointSize += 5
+        font = font.Bold()
+        text.SetFont(font)
+        return text
 
-    def build(self):
-        Window.clearcolor = (.1, .1, .1, 1)
-        p = ROOT_DIR.joinpath('resources', 'icon.png')
-        self.icon = str(p)
-        return self.qrFrame
+    def _create_sizer(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self._panel.SetSizer(sizer)
+        return sizer
+
+    def static_bitmap_from_pil_image(self, pil_image):
+        wx_image = wx.Image(pil_image.size[0], pil_image.size[1])
+        wx_image.SetData(pil_image.convert("RGB").tobytes())
+
+        bitmap = wx.Bitmap(wx_image)
+        static_bitmap = wx.StaticBitmap(self._panel, wx.ID_ANY, wx.NullBitmap)
+        static_bitmap.SetBitmap(bitmap)
+        return static_bitmap
 
 
 def qr_window(code, at_close=None):
@@ -85,4 +67,7 @@ def qr_window(code, at_close=None):
 
 if __name__ == '__main__':
     img = Image.open('../resources/icon.png')
-    qr_window(img)
+    app = wx.App()
+    frame = QrApp(img, None, title='QrTransfer')
+    frame.Show()
+    app.MainLoop()
