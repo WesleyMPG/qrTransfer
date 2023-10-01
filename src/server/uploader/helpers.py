ae5ec22d723695afcc9a922cad0df27f7a43de7f
wesley silva <wesley.mprog@gmail.com>
2023-10-01 02:40:40 -0300

    Improve uploader helper PortProvider tests

--- src/server/uploader/helpers.py
+++ src/server/uploader/helpers.py
@@ -6,23 +6,29 @@ class PortProvider:
     MAX_PORT_TRIES = 10
 
     def __init__(self):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock = None
 
     def get_random_port(self):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port = self._get_random_port()
+        self._sock.close()
+        if port: return port
+        raise UnableToGetAvailablePortError()
+
+    def _get_random_port(self):
         for i in range(self.MAX_PORT_TRIES):
             port = randint(5001, 9999)
-            if self._is_port_available(port): return port
-        self._sock.close()
-        raise UnableToGetAvailablePortError()
+            if self._is_port_available(port): break
+            port = None
+        return port
 
     def _is_port_available(self, port):
         try:
             self._sock.connect_ex(('localhost', port))
-            self._sock.close()
             return True
         except socket.error:
             return False
-        
+
 
 class UnableToGetAvailablePortError(Exception):
     def __init__(self, *args, **kwargs):
--- src/test/server_test/test_port_provider.py
+++ src/test/server_test/test_port_provider.py
@@ -1,18 +1,34 @@
+from subprocess import Popen
+from time import sleep
 import pytest
 from server.uploader.helpers import PortProvider, UnableToGetAvailablePortError
 
 
-def test_get_random_port_success(mocker):
-    pp = PortProvider()
-    mocker.patch('server.uploader.helpers.PortProvider._is_port_available', return_value=True)
-    try:
-        pp.get_random_port()
-    except Exception as ex:
-        assert False, f"Failed to get random port: {ex}"
+@pytest.fixture
+def port_provider():
+    return PortProvider()
 
 
-def test_get_random_port_failure(mocker):
-    pp = PortProvider()
-    mocker.patch('server.uploader.helpers.PortProvider._is_port_available', return_value=False)
+def test_port_is_numeric(port_provider):
+    port = port_provider.get_random_port()
+    assert isinstance(port, int)
+
+
+def test_port_is_random(port_provider):
+    port1 = port_provider.get_random_port()
+    port2 = port_provider.get_random_port()
+    assert port1 != port2
+
+
+def test_retry_if_randomized_port_is_in_use(mocker, port_provider):
+    mocker.patch('server.uploader.helpers.randint', side_effect=[8080, 5001])
+    mocker.patch('server.uploader.helpers.PortProvider._is_port_available', side_effect=[False, True])
+    port = port_provider.get_random_port()
+    assert port == 5001
+
+
+def test_get_random_port_failure_on_max_port_tries_reached(mocker, port_provider):
+    mock = mocker.patch('server.uploader.helpers.PortProvider._is_port_available', return_value=False)
     with pytest.raises(UnableToGetAvailablePortError) as e:
-        pp.get_random_port()
+        port_provider.get_random_port()
+        assert mock.call_count == PortProvider.MAX_PORT_TRIES
